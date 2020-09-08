d435e767ea37518d080eec0de52a6a2f0c170e1a
edwin.wang <edwin.wang@movit-tech.com>
2020-09-08 11:19:18 +0800

    httpRequest请求日志打印 版本号更新

--- module-web/src/main/java/com/github/xuchen93/web/common/HttpPackUtil.java
+++ module-web/src/main/java/com/github/xuchen93/web/common/HttpPackUtil.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.Method;
+import com.github.xuchen93.web.common.model.PackHttpRequest;
 
 public final class HttpPackUtil {
     private static String localUrl = "http://localhost:8080/";
@@ -32,17 +33,17 @@ public final class HttpPackUtil {
     }
 
     public static HttpRequest createRequest(Method method, String url) {
-        HttpRequest request = new HttpRequest(localUrl + url).method(method);
+        HttpRequest request = new PackHttpRequest(localUrl + url).method(method);
         return setToken(request);
     }
 
     public static HttpRequest createGet(String url) {
-        HttpRequest request = HttpRequest.get(localUrl + url);
+        HttpRequest request = new PackHttpRequest(localUrl + url).method(Method.GET);
         return setToken(request);
     }
 
     public static HttpRequest createPost(String url) {
-        HttpRequest request = HttpRequest.post(localUrl + url);
+        HttpRequest request = new PackHttpRequest(localUrl + url).method(Method.POST);
         return setToken(request);
     }
 
