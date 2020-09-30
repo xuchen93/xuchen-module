074dcc13f4c1a366fe527b71c65c386cd8c55242
wangxuchen <599268367@qq.com>
2020-09-30 14:28:52 +0800

    bug,升级版本号

--- module-database/src/main/java/com/github/xuchen93/database/config/MybatisPlusConfig.java
+++ module-database/src/main/java/com/github/xuchen93/database/config/MybatisPlusConfig.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 
 @Slf4j
 @Configuration
-@MapperScan("pers.xuchen.module.database.table.dao")
+@MapperScan("com.github.xuchen93.database.table.dao")
 public class MybatisPlusConfig {
 
     /**
